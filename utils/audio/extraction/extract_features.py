4e825ea58eb79478c706d81cf1dc5e4dacf83cda
AnimaVR <22724774+AnimaVR@users.noreply.github.com>
2025-02-06 15:24:26 +0000

    Update extract_features.py

--- utils/audio/extraction/extract_features.py
+++ utils/audio/extraction/extract_features.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 from utils.audio.load_audio import load_and_preprocess_audio, load_audio_from_bytes
-from utils.audio.extraction.extract_features_utils import  smooth_features, extract_mfcc_features
+from utils.audio.extraction.extract_features_utils import  smooth_features, extract_mfcc_features, extract_autocorrelation_features,
 
 def extract_audio_features(audio_input, sr=88200, from_bytes=False):
     if from_bytes:
@@ -23,15 +23,21 @@ def extract_audio_features(audio_input, sr=88200, from_bytes=False):
     
     return combined_features, y
 
-def extract_and_combine_features(
-    y, sr, frame_length, hop_length, 
-    apply_smoothing=False,
-):
+def extract_and_combine_features(y, sr, frame_length, hop_length, apply_smoothing=False, include_autocorr=False):
+   
     all_features = []
     
+    # 1) MFCC as baseline
     mfcc_features, _ = extract_mfcc_features(y, sr, frame_length, hop_length)
     all_features.append(mfcc_features)
-
+    
+    # 2) Autocorrelation
+    if include_autocorr:
+        autocorr_features = extract_autocorrelation_features(
+            y, sr, frame_length, hop_length
+        )
+        all_features.append(autocorr_features)
+    
     combined_features = np.hstack(all_features)
 
     if apply_smoothing:
